47483fcd08d24de4dfb73696d8a03d8ee3b19529
Dhaneshwar Kumar <dhannesh@gmail.com>
2024-02-19 07:05:05 +0530

    database file added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to Book Store Project");
